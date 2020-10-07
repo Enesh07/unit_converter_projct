178b6cdec05691662901b710886c3d84e7bea9a2
Enesh07 <eneshorazova21@gmail.com>
2020-10-07 16:23:13 +0500

    unit converter projects

---
+++ script.js
@@ -0,0 +1,14 @@
+const kelvin = 293;//that explain this line code
+let Celsius = kelvin- 273;
+let Fahrenheit = Math.round(Celsius *(9/5) +32);//that fahren
+
+
+alert(`The temperature is ${Fahrenheit} degrees Fahrenheit .`)
+let Newton = Math.floor(Celsius *(33/100));//that newton
+console.log(`The temperature is ${Fahrenheit} degrees Fahrenheit, which equal the temperature in ${Newton} degrees Newton .`);
+alert(`The temperature is ${Newton} degrees Newton.`)
+
+
+console.log(kelvin);
+console.log(Celsius);
+console.log(Fahrenheit);
